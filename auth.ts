dc08ae12132376de4a5973850340f79389f11fd8
Weina Dai <weinadai@Weinas-Laptop.lan>
2025-02-08 16:19:01 -0500

    working chats

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import 'server-only'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+//import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers'
 
 export const auth = async ({
@@ -8,9 +9,26 @@ export const auth = async ({
   cookieStore: ReturnType<typeof cookies>
 }) => {
   // Create a Supabase client configured to use cookies
-  const supabase = createServerComponentClient({
-    cookies: () => cookieStore
-  })
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          } catch (error) {
+            console.log(error)
+          }
+        },
+      },
+    }
+  )
   const { data, error } = await supabase.auth.getSession()
   if (error) throw error
   return data.session
